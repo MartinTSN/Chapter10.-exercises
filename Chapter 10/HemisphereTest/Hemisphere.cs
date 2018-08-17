e4fa11d277534502c9dc23757d76ca8fe3be382c
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-17 09:35:12 +0200

    Made Math

--- Chapter 10/HemisphereTest/Hemisphere.cs
+++ Chapter 10/HemisphereTest/Hemisphere.cs
@@ -1,6 +1,43 @@
-﻿class Hemisphere
-{
-    int radius = 1;
+﻿using System;
 
-    
+class Hemisphere
+{
+    decimal radius = 1;
+
+    public decimal Volume
+    {
+        get
+        {
+            return Decimal.Multiply(Decimal.Multiply(4 / 3, (decimal)Math.PI), Radius);
+        }
+    }
+    public decimal CurvedSurfaceArea
+    {
+        get
+        {
+            return Decimal.Multiply(Decimal.Multiply(2, (decimal)Math.PI), Decimal.Multiply(Radius, 2));
+        }
+    }
+    public decimal TotalSurfaceArea
+    {
+        get
+        {
+            return Decimal.Multiply(Decimal.Multiply(3, (decimal)Math.PI), Decimal.Multiply(Radius, 2));
+        }
+    }
+
+    public decimal Radius
+    {
+        get
+        {
+            return radius;
+        }
+        set
+        {
+            if (value > 0.0m || value < 12.0m)
+            {
+                radius = value;
+            }
+        }
+    }
 }
