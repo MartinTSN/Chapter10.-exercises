3bbc3a7c4908f62d14b882fe29b8ba15fc7f39ee
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-20 10:20:54 +0200

    Finished (?) DepreciatingValue and started the test

--- Chapter 10/DepreciatingValueTest/DepreciatingValue.cs
+++ Chapter 10/DepreciatingValueTest/DepreciatingValue.cs
@@ -1,4 +1,40 @@
 ﻿class DepreciatingValue
 {
+    private static decimal annualDepreciationRate;
+    private decimal valueOfAsset;
 
+    public DepreciatingValue(decimal valueOfAsset = 0, decimal annualDepreciationRate = 10)
+    {
+        ValueOfAsset = valueOfAsset;
+    }
+
+
+    public static decimal AnnualDepreciationRate
+    {
+        get
+        {
+            return annualDepreciationRate;
+        }
+        set
+        {
+            annualDepreciationRate = value;
+        }
+    }
+
+    public decimal ValueOfAsset
+    {
+        get
+        {
+            return valueOfAsset;
+        }
+        set
+        {
+            valueOfAsset = value;
+        }
+    }
+
+    public decimal CalculateAnnualDepreciation()
+    {
+        return ValueOfAsset * AnnualDepreciationRate;
+    }
 }
--- Chapter 10/DepreciatingValueTest/DeptreciatingValueTest.cs
+++ Chapter 10/DepreciatingValueTest/DeptreciatingValueTest.cs
@@ -4,6 +4,14 @@ class DeptreciatingValueTest
 {
     static void Main()
     {
+        var asset1 = new DepreciatingValue(5000000);
+        var asset2 = new DepreciatingValue(6000000);
+        var asset3 = new DepreciatingValue(7000000);
 
+        Console.WriteLine(asset1.ValueOfAsset);
+
+
+
+        Console.ReadLine();
     }
 }
